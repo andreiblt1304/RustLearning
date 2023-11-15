428eb341e137b33656573c689999a515372dea5d
Andrei Baltariu <andrei.baltariu@bearingpoint.com>
2023-11-15 10:44:55 +0200

    Added tree with smart pointers project

---
+++ tree_smart_pointers/src/main.rs
@@ -0,0 +1,33 @@
+use std::cell::RefCell;
+use std::rc::{Rc, Weak};
+
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    parent: RefCell<Weak<Node>>,
+    children: RefCell<Vec<Rc<Node>>>,   
+    // it will own its children
+    // it will share ownership with variables 
+    // => we can access each Node in the tree directly
+}
+
+fn main() {
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
+    let branch = Rc::new(Node {
+        value: 5,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![Rc::clone(&leaf)]), 
+        // Node in leaf has 2 owners: leaf, branch
+    });
+
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade().unwrap());
+}
